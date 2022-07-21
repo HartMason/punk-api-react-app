b774c5687e52e172284700aafa7c32bc35108d86
HartMason <hartmason85@gmail.com>
2022-07-21 18:21:20 -0500

    axios request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { Component } from "react";
 import "./App.css";
+import axios from 'axios'
+import { render } from "@testing-library/react";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component() {
+  constructor() {
+    super()
+  this.state = {
+    arrayOfBeer: []
+  }
+  }
+
+  axios.get('https://api.punkapi.com/v2/beers')
+    .then( res => {
+      const arrayOfBeer = res.data 
+      this.setState({ arrayOfBeer })
+    })
+
+
+render() {
+  return <div className="App">
+
+  </div>;
 }
-
+}
 export default App;
