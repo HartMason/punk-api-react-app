866df7ebc79f4adaba0bdd91220ee0128aa5f8df
HartMason <hartmason85@gmail.com>
2022-07-21 20:58:10 -0500

    need to implement like button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import axios from "axios";
 import { render } from "@testing-library/react";
 import BeerCard from "./BeerCard";
 
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -13,24 +12,38 @@ class App extends Component {
     };
   }
 
+  handleLike() {
+    console.log("LIKED!!!!!!!!!!");
+    return alert("is Liked");
+  }
+
   componentDidMount() {
     axios.get("https://api.punkapi.com/v2/beers").then((res) => {
-      const arrayOfBeer = res.data;
-      console.log(arrayOfBeer)
+      const arrayOfBeer = res.data.map((beer) => {
+        beer.likeCount = 0;
+        return beer;
+      });
+      console.log(arrayOfBeer);
       this.setState({ arrayOfBeer });
     });
   }
 
   render() {
-    return <div className="App">
-    <header className="App-header">
-      
-     <ol>{this.state.arrayOfBeer.map((beer, index) => (
-      <BeerCard key={index} name={beer.name} image_url={beer.image_url}/>
-     ))}</ol>
-     
-    </header>
-    </div>;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <ol>
+            {this.state.arrayOfBeer.map((beer, index) => (
+              <BeerCard
+                key={index} //passing props or display a value...???? Template literalls 
+                name={beer.name}
+                image_url={beer.image_url}
+              />
+            ))}
+          </ol>
+        </header>
+      </div>
+    );
   }
 }
 export default App;
--- src/BeerCard.js
+++ src/BeerCard.js
@@ -2,11 +2,19 @@ import React from 'react'
 
 
 const BeerCard = (props) => {
+
+    handleLike = () => {
+        console.log("LIKED!!!!!!!!!!");
+        return alert('is Liked')
+      };
+    
   return (
     <div>
         <h1>{props.tagline}</h1>
         <li>{props.name}</li>
         <img src={props.image_url}></img>
+        <button onClick={handleLike}> {props.likeCount} Like</button>
+        
     </div>
   )
 }
