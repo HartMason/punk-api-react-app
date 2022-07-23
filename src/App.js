944841b900c43dbfac978b75df8c89631dc9f17e
HartMason <hartmason85@gmail.com>
2022-07-23 13:37:40 -0500

    need css styling flex direction row

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import axios from "axios";
 import { render } from "@testing-library/react";
 import BeerCard from "./BeerCard";
 
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -13,30 +12,25 @@ class App extends Component {
     };
   }
 
-  // handleLike() {                      //Work on Button first thing tomorrow
-  //   console.log("LIKED!!!!!!!!!!");
-  //   return alert("is Liked");
-  // }
-
   componentDidMount() {
     axios.get("https://api.punkapi.com/v2/beers").then((res) => {
       const arrayOfBeer = res.data.map((beer) => {
         beer.likeCount = 0;
         return beer;
       });
-      console.log(arrayOfBeer);
+      console.log(arrayOfBeer, "ARRAY OF BEERS");
       this.setState({ arrayOfBeer });
     });
   }
 
-  render() {                            
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <ol>
             {this.state.arrayOfBeer.map((beer, index) => (
               <BeerCard
-                key={index} //passing props or display a value...???? Template literalls // displa
+                key={index} //passing props or display a value...???? Template literalls // display
                 name={beer.name}
                 image_url={beer.image_url}
               />
--- src/BeerCard.js
+++ src/BeerCard.js
@@ -2,20 +2,18 @@ import React from "react";
 import BeerCard from "./BeerCard.css";
 
 const beerCard = (props) => {
-  // handleLike = () => {
-  //     console.log("LIKED!!!!!!!!!!");
-  //     return alert('is Liked')
-  //   };
+
+    //ask Jacob how to style data fetched from the api !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
   return (
-    <div className="parentContainer">
+    
       <div className="beerContainer">
         <li className="beerName">{props.name}</li>
         <img className="beerImage" src={props.image_url}></img>
         <h1>{props.tagline}</h1>
         {/* <button onClick={handleLike}> {props.likeCount} Like</button> */}
       </div>
-    </div>
+   
   );
 };
 
