d21831a4d35312868ca37ff966247420c32923e5
HartMason <hartmason85@gmail.com>
2022-07-26 19:00:30 -0500

    added like button

--- src/BeerCard.js
+++ src/BeerCard.js
@@ -1,22 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./beerCard.css";
 
-const beerCard = (props) => {
-
-    //ask Jacob how to style data fetched from the api !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+const BeerCard = (props) => {
+  const [like, setlike] = useState(false);
+  const results = useState(false);
+  console.log(results);
 
   return (
-    
-      <div className="beerContainer">
-        <ul className="beerName">{props.name}</ul>
-        <ul className="flex">
-        <img className="beerImage" src={props.image_url} alt='beerImage'></img>
-        </ul>
-        <h1>{props.tagline}</h1>
-        {/* <button onClick={handleLike}> {props.likeCount} Like</button> */}
-      </div>
-   
+    <div className="beerContainer">
+      <ul className="beerName">{props.name}</ul>
+      <ul className="flex">
+        <img className="beerImage" src={props.image_url} alt="beerImage"></img>
+      </ul>
+      <h1>{props.tagline}</h1>
+      <button onClick={() => setlike(!like)}>
+        {like ? "Like Me" : "Not Liked"}
+      </button>
+    </div>
   );
 };
 
-export default beerCard;
+export default BeerCard;
